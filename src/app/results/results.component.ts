62c655cbad77b11e6f4080f623e15cec91cba629
bryan <bryanmichaelbrown@gmail.com>
2020-09-17 11:25:30 -0400

    activitiesAPI

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -11,13 +11,13 @@ import { analyzeFileForInjectables } from '@angular/compiler';
 export class ResultsComponent implements OnInit {
   @Input() info: any;
   restaurant: any;
-  activity: any;
+  activity: any = [];
   barOrCoffee: any;
   barOrCoffeeGoogle: any;
   restaurantGoogle: any;
-  activityGoogle: any;
+  activityGoogle: any = [];
 
-  constructor(private service: DataService, private route: ActivatedRoute) { }
+  constructor(private service: DataService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((params: any) => {
@@ -25,11 +25,10 @@ export class ResultsComponent implements OnInit {
       this.getRestaurants(params.params);
       this.getBars(params.params);
       this.getActivities(params.params);
-
     });
   }
 
-  displayRestaurant = (restaurant) => {
+  displayRestaurant = (restaurant: any) => {
     console.log(restaurant);
     this.service.retrievePlace(restaurant).subscribe((response) => {
       console.log(response);
@@ -41,11 +40,10 @@ export class ResultsComponent implements OnInit {
     this.service.getRestaurants(params).subscribe((response) => {
       console.log(response);
       this.restaurant = response;
-      this.displayRestaurant(this.restaurant[0].placeid);
-      console.log(this.restaurant);
+      this.displayRestaurant(this.restaurant[0].place_id);
+      console.log(this.restaurant[0].place_id);
     });
   };
-
 
   displayBarsOrCoffee = (bar: any) => {
     console.log(bar);
@@ -55,14 +53,13 @@ export class ResultsComponent implements OnInit {
     });
   };
 
-  displayActivity = (activity: any) => {
-    console.log(activity);
-    this.service.retrievePlace(activity).subscribe((response) => {
-      console.log(response);
-      this.activityGoogle = response;
-    });
-  };
-
+  // displayActivity = (activity: any) => {
+  //   console.log(activity);
+  //   this.service.retrievePlace(activity).subscribe((response) => {
+  //     console.log(response);
+  //     this.activityGoogle = response;
+  //   });
+  // };
 
   getBars = (params) => {
     this.service.getBars(params).subscribe((response) => {
@@ -71,12 +68,31 @@ export class ResultsComponent implements OnInit {
       this.displayBarsOrCoffee(this.barOrCoffee[0].place_id);
     });
   };
+  displayActivity = (activity: any) => {
+    console.log(activity);
+    this.service.retrievePlace(activity).subscribe((response) => {
+      console.log(response);
+      this.activityGoogle.push(response);
+      console.log(this.activityGoogle);
+    });
+  };
 
   getActivities = (params) => {
     this.service.getActivities(params).subscribe((response) => {
       console.log(response);
       this.activity = response;
-      this.displayActivity(this.activityGoogle[0].place_id);
+      this.activity.forEach((item) => {
+        this.displayActivity(item.place_id);
+      });
     });
   };
+
+  // getActivities = (params) => {
+  //   this.service.getActivities(params).subscribe((response) => {
+  //     console.log(response);
+  //     this.activity = response;
+  //     this.displayActivity(this.activityGoogle.place_id);
+  //     console.log(this.activity[0].place_id);
+  //   });
+  // };
 }
